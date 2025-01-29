756c3efad48df95ff57f4b171954999cb2701b4e
Benlahcen <benlahcen.assia@gmail.com>
2025-01-29 20:49:58 +0100

    coms explicatifs

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 // Sélectionne l'élément d'entrée où l'utilisateur saisit le mot de passe dans la boîte de mot de passe  
 const passwordInput = document.querySelector(".password-box input"),
+// Sélectionne l'icône de copie dans la boîte de mot de passe (probablement pour copier le mot de passe)
 copyIcon = document.querySelector(".password-box .copy-icon"),
+// Sélectionne l'élément d'entrée de la barre de plage (probablement pour définir la longueur du mot de passe)
 rangeInput = document.querySelector(".range-box input"),
+// Sélectionne l'élément qui affiche le nombre associé au curseur de la barre de plage
 sliderNumber = document.querySelector(".range-box .slider-number"),
+// Sélectionne le bouton qui sera utilisé pour générer un nouveau mot de passe  
 generateButton = document.querySelector(".generate-button");
 
 let allCharacters = "abcdefghijklmnopanstuvwxyZABCDEFGHIJKLMNOPORSTUWXYZ0123456789↑！$%&｜［］（）｛｝：；・*＋-#@＜>";
